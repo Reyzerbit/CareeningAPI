4c33ddd8cb1e457d6c31f6545fc681cfc575fbe2
Jacob Batista <jacobaccio@gmail.com>
2024-08-23 13:06:36 -0600

    More Stuff

--- src/main/java/com/anachronystudios/careening/api/Util.java
+++ src/main/java/com/anachronystudios/careening/api/Util.java
@@ -1,5 +1,8 @@
 package com.anachronystudios.careening.api;
 
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+
 public class Util
 {
 	public static String stackTraceArrayToString(StackTraceElement[] e)
@@ -11,4 +14,16 @@ public class Util
 
 	public static String throwableToString(Throwable e) { return stackTraceArrayToString(e.getStackTrace()); }
 	public static String formatThrowable(String userDefinedMessage, Throwable e) { return userDefinedMessage + " - " + e.getMessage() + "\n" + throwableToString(e); }
+	
+	public class MediaUtil
+	{
+		public static void setLooping(MediaPlayer player)
+		{
+			player.setOnEndOfMedia(() ->
+			{
+				player.seek(Duration.ZERO);
+				player.play();
+			});
+		}
+	}
 }
--- src/main/java/com/anachronystudios/careening/api/logging/SystemErrorProxyStream.java
+++ src/main/java/com/anachronystudios/careening/api/logging/SystemErrorProxyStream.java
@@ -1,51 +1,19 @@
 package com.anachronystudios.careening.api.logging;
 
 import java.io.PrintStream;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.slf4j.Logger;
 
 public class SystemErrorProxyStream extends PrintStream
 {
-	private List<String> stringCache = new ArrayList<String>();
-	private LocalDateTime lastPrint = LocalDateTime.MIN;
-	private Thread errorProxyThread;
+	public SystemErrorProxyStream(PrintStream stream, Logger log) { super(stream); }
 	
-	public SystemErrorProxyStream(PrintStream stream, Logger log)
-	{
-		super(stream);
-		
-		errorProxyThread = new Thread(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				while(true)
-				{
-					if(!lastPrint.equals(LocalDateTime.MIN) && Duration.between(lastPrint, LocalDateTime.now()).getNano() > 250000000)
-					{
-						String errString = String.join("\n", stringCache);
-						log.error(errString);
-						Log.handleError(errString);
-						break;
-					}
-				}
-			}
-		});
-	}
-	
-	public void println(final String string) { print(string); }
+	public void println(final String string) { print(string + "\n"); }
 
 	public void print(final String string)
 	{
 		if(string.toLowerCase().contains("jprofiler")) return;
 		
-		if(!errorProxyThread.isAlive()) errorProxyThread.start();
-		
-		stringCache.add(string);
-		lastPrint = LocalDateTime.now();
+		Log.getLogger().error(string);
 	}
 }
